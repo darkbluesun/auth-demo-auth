5f7be2ce596c5f5f7629b41ad90733e157d0dd28
Chris Young <chris.young@brighte.com.au>
2020-08-30 22:47:04 +1000

    Use a session to persist the login

--- src/main/kotlin/com/chrisyoung/auth/LoginController.kt
+++ src/main/kotlin/com/chrisyoung/auth/LoginController.kt
@@ -7,18 +7,27 @@ import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.http.HttpServletRequest
 
 @Controller
 class LoginController(private val userRepository: UserRepository) {
     @GetMapping("/login")
-    fun loginForm(model: Model): String {
+    fun loginForm(model: Model, request: HttpServletRequest): String {
         model["title"] = "Login"
         model["error"] = ""
+        val session = request.session
+        val user: User? = session.getAttribute("user") as User?
+        if (user != null) {
+            model["username"] = user.username
+            model["password"] = user.password
+            return "loggedin"
+        }
         return "login"
     }
     @PostMapping("/login", consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
     fun login(
             model: Model,
+            request: HttpServletRequest,
             @RequestParam(name = "username") username: String,
             @RequestParam(name = "password") password: String
     ): String {
@@ -28,6 +37,9 @@ class LoginController(private val userRepository: UserRepository) {
         model["password"] = password;
         val user = userRepository.findByUsername(username)
         if (user !== null && password == user.password) {
+            val session = request.session
+            session.setAttribute("username", username)
+            session.setAttribute("user", user)
             return "loggedin"
         } else {
             model["error"] = "user not found or password incorrect"
